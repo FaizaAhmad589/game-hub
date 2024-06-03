b412a7577ca043ffb2465cc8dcfdbac816dbc33d
Faiza <faizaahmad589@gmail.com>
2024-06-03 23:19:23 +0500

    Filter games by genre

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,33 +1,42 @@
-import {FaWindows, FaPlaystation, FaXbox, FaApple,FaLinux, FaAndroid} from "react-icons/fa"
-import {MdPhoneIphone} from "react-icons/md"
-import {SiNintendo} from "react-icons/si"
-import {BsGlobe} from "react-icons/bs"
-import { platform } from '../hooks/useGames'
-import { HStack, Icon, Text, color } from '@chakra-ui/react'
-import { IconType } from "react-icons"
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { platform } from "../hooks/useGames";
+import { HStack, Icon, Text, color } from "@chakra-ui/react";
+import { IconType } from "react-icons";
 
-interface Props{
-    platforms : platform[]
+interface Props {
+  platforms: platform[];
 }
 
-const PlatformIconList = ({platforms}: Props) => {
-      const IconMap : {[key : string] : IconType} = {
-        pc : FaWindows,
-        playstation : FaPlaystation,
-        xbox : FaXbox,
-        nintendo : SiNintendo,
-        mac : FaApple,
-        linux : FaLinux,
-        android : FaAndroid,
-        ios : MdPhoneIphone,
-        web : BsGlobe
-      }
+const PlatformIconList = ({ platforms }: Props) => {
+  const IconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
 
   return (
     <HStack marginY={1}>
-    {platforms.map( (platform) => <Icon as={IconMap[platform.slug] }  color="gray.500" />)}
+      {platforms.map((platform) => (
+        <Icon key={platform.id} as={IconMap[platform.slug]} color="gray.500" />
+      ))}
     </HStack>
-  )
-}
+  );
+};
 
-export default PlatformIconList
+export default PlatformIconList;
