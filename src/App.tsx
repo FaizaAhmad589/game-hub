87a76c293b34bc95c1fa3f438300c9a82083b9be
Faiza <faizaahmad589@gmail.com>
2024-06-04 18:23:30 +0500

    Built platform selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,42 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react"
-import NavBar from "./components/NavBar"
-import GameGrid from "./components/GameGrid"
-import GenreList from "./components/GenreList"
-import { useState } from "react"
-import { Genre } from "./hooks/useGenres"
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
+import { useState } from "react";
+import { Genre } from "./hooks/useGenres";
+import PlatformSelector from "./components/PlatformSelector";
 
 function App() {
-   const [selectedGenre,setSelectedGenre]= useState<Genre | null>(null);
+  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
 
-  return <Grid  templateAreas={{
-    base : `"nav" "main"`,
-    lg: `"nav nav" "aside main"` //for devices that are wider than 1024px
-  }}
-    templateColumns={{
-      base: "1fr",
-      lg: "200px 1fr"
-    }}
-  >
-    <GridItem area= "nav" ><NavBar/></GridItem>
-    <Show above="lg">
-    <GridItem paddingX={5} area= "aside" ><GenreList selectedGenre={selectedGenre} onSelectGenre={(genre)=> setSelectedGenre(genre)}/></GridItem>
-    </Show>
-    <GridItem area="main"> <GameGrid selectedGenre={selectedGenre}/></GridItem>
-  </Grid>
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, //for devices that are wider than 1024px
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem paddingX={5} area="aside">
+          <GenreList
+            selectedGenre={selectedGenre}
+            onSelectGenre={(genre) => setSelectedGenre(genre)}
+          />
+        </GridItem>
+      </Show>
+      <GridItem area="main">
+        <PlatformSelector/>
+        <GameGrid selectedGenre={selectedGenre} />
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
- 
+export default App;
